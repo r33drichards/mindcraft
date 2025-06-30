ca06804d9d6e02b0d2ac8d10549a5837fe693682
Your Name <you@example.com>
2025-06-30 04:58:02 +0000

    add build deps

--- flake.nix
+++ flake.nix
@@ -12,9 +12,17 @@
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
     
     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
+      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
         nixpkgs.legacyPackages.x86_64-linux.python3
         nixpkgs.legacyPackages.x86_64-linux.nodejs
+        pkg-config
+        xorg.libX11
+        xorg.libXi
+        xorg.libXext
+        xorg.libX11.dev
+        xorg.libXi.dev
+        xorg.libXext.dev
+
       ];
     };
 
